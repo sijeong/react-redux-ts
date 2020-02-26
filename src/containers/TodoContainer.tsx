6c9b29f53e6d4c7d4bf04721b0a47491af17c977
sijeong <sijeong@outlook.com>
2020-02-26 11:45:08 +0900

    in case for forget some FC concepts

--- src/containers/TodoContainer.tsx
+++ src/containers/TodoContainer.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, Dispatch } from 'react';
 import { connect } from 'react-redux';
 import * as MyTypes from 'MyTypes'
+import { actionTypes } from '../actions';
+import { TodoItem } from '../components/TodoItem';
 interface TodoContainerState {
     todoInput: string
 }
@@ -8,32 +10,45 @@ interface TodoContainerState {
 interface TodoContainerProps {
     count: number;
     todoList: string[];
-    addToDo: (item: string) => object;
-    deleteTodo: (index: number) => object;
+    addToDo: (item: string) => void;
+    deleteTodo: (index: number) => void;
 }
 
-const TodoContainer: React.FC = () => {
+const TodoContainer: React.FC<TodoContainerProps> = ({ count, todoList, addToDo, deleteTodo }) => {
     const [state, setState] = useState({
         todoInput: ''
     });
 
-    const handleTextChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setState({
             todoInput: e.target.value
         })
     }
 
     const handleButtonClick = () => {
-
+        addToDo(state.todoInput);
+        setState({ todoInput: '' })
     }
 
-    const handleDeleteButtonClick = () => {
-
+    const handleDeleteButtonClick = (index: number) => {
+        console.log('deleting', index);
+        deleteTodo(index)
     }
 
+    let todoJSX: JSX.Element[] | JSX.Element;
+    if (!todoList.length) {
+        todoJSX = <p>Not to do</p>
+    } else {
+        todoJSX = todoList.map((item, index) => {
+            return (
+                <TodoItem item={item} key={index} index={index} handleDelete={handleDeleteButtonClick} />
+            )
+        })
+    }
     return (
+
         <>
-            <h1>React Redux Typescript</h1>
+            {todoJSX}
 
         </>
     )
@@ -41,14 +56,15 @@ const TodoContainer: React.FC = () => {
 
 const MapStateToProps = (store: MyTypes.ReducerState) => {
     return {
-        count: store.todo.count,
-        todoList: store.todo.list
+        count: store.todo.count as number,
+        todoList: store.todo.list as string[]
     }
 }
 
-const MapDispatchToProps = () => {
-
-}
+const MapDispatchToProps = (dispatch: Dispatch<MyTypes.RootAction>) => ({
+    addToDo: (item: string) => dispatch({ type: actionTypes.ADD, payload: item }),
+    deleteTodo: (index: number) => dispatch({ type: actionTypes.DELETE, payload: index })
+})
 
 export default connect(
     MapStateToProps,
